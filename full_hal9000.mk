efc0b81de7e6854f3482caf7839a8408a1e434b4
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-10-24 20:35:20 +0200

    Initial import.

---
+++ full_hal9000.mk
@@ -0,0 +1,5 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Overrides
+PRODUCT_NAME := full_hal9000
+PRODUCT_DEVICE := hal9000
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+add_lunch_combo full_hal9000-userdebug
