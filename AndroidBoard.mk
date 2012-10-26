cfd5751e6909c2753ac9028ae876fe7b82d80d24
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-10-26 18:28:26 +0200

    Add rule to copy kernel.
    The kernel is useless now.

---
+++ AndroidBoard.mk
@@ -0,0 +1,16 @@
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+endif
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+# this is here to use the pre-built kernel
+
