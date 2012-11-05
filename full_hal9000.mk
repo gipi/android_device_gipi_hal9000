ea3b65a3ddb873bdae6748ab87459b589db173d7
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-11-05 10:20:33 +0100

    Add commented out BUILD_TINY_ANDROID.

--- full_hal9000.mk
+++ full_hal9000.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# from build/core/main.mk
+#BUILD_TINY_ANDROID := true
+
 # Overrides
 PRODUCT_NAME := full_hal9000
 PRODUCT_DEVICE := hal9000
